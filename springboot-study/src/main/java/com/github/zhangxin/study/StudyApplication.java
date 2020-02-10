184cfd74c3875e73e0b219063520b48aebad462a
zhangxin1219 <1290007051@qq.com>
2020-02-10 19:50:48 +0800

    新增springboot模块

---
+++ springboot-study/src/main/java/com/github/zhangxin/study/StudyApplication.java
@@ -0,0 +1,15 @@
+package com.github.zhangxin.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+}
---
+++ springboot-study/src/main/java/com/github/zhangxin/study/config/BeanConfig.java
@@ -0,0 +1,12 @@
+package com.github.zhangxin.study.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * @Auther: Zhang Xin
+ * @Date: 2020/2/10 11:15
+ * @Description:
+ */
+@SpringBootConfiguration
+public class BeanConfig {
+}
